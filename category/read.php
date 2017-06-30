ed2ff7a07055e6a8443ab1323a3d1844d9d54c4e
harshkhandelwalcsit <you@example.comharsh.khandelwal@ranosys.com>
2017-06-30 14:35:57 +0530

    Add Functionality to retrive data by Id

--- category/read.php
+++ category/read.php
@@ -1,29 +1,29 @@
 <?php
-// required headers
+// required header
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
  
 // include database and object files
 include_once '../config/database.php';
-include_once '../objects/product.php';
+include_once '../objects/category.php';
  
-// instantiate database and product object
+// instantiate database and category object
 $database = new Database();
 $db = $database->getConnection();
  
 // initialize object
-$product = new Product($db);
+$category = new Category($db);
  
-// query products
-$stmt = $product->read();
+// query categorys
+$stmt = $category->read();
 $num = $stmt->rowCount();
  
 // check if more than 0 record found
 if($num>0){
  
     // products array
-    $products_arr=array();
-    $products_arr["records"]=array();
+    $categories_arr=array();
+    $categories_arr["records"]=array();
  
     // retrieve our table contents
     // fetch() is faster than fetchAll()
@@ -34,19 +34,16 @@ if($num>0){
         // just $name only
         extract($row);
  
-        $product_item=array(
+        $category_item=array(
             "id" => $id,
             "name" => $name,
-            "description" => html_entity_decode($description),
-            "price" => $price,
-            "category_id" => $category_id,
-            "category_name" => $category_name
+            "description" => html_entity_decode($description)
         );
  
-        array_push($products_arr["records"], $product_item);
+        array_push($categories_arr["records"], $category_item);
     }
  
-    echo json_encode($products_arr);
+    echo json_encode($categories_arr);
 }
  
 else{
